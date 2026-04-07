0df3e68bf49063f527b5d8351b700c565aa755e7
debajo <debdeepm89@gmail.com>
2026-04-07 18:27:34 +0530

    Fix comments and variable names in higherOrder.js

--- src/modules/functions/higherOrder.js
+++ src/modules/functions/higherOrder.js
@@ -8,20 +8,20 @@ function fun2(action) {
 
 fun2(fun);
 
-//Built-in HOF- map()
+//Built-in HOF- map() | gets a new array out of it with transformed ones
 const sample = [1, 2, 3, 4, 5];
 const square = sample.map((num) => num * num);
 console.log(square);
 //[1,4,9,16,25]
 
 
-//Built-in HOF- filter()
+//Built-in HOF- filter() | gets a new array out of it with filtered elements
 const arr = [1, 2, 3, 4, 5];
-const even = n.mafilterp((num) => num %2 === 0);
+const even = arr.filter((num) => num %2 === 0);
 console.log(even);
 //[2,4]
 
-//Built-in HOF- reduce()
+//Built-in HOF- reduce() | gets a new array out of it
 const elements = [1, 2, 3, 4, 5];
 const sum = elements.reduce((acc, curr) => acc + curr, 0)
 console.log(sum);
@@ -33,7 +33,7 @@ iterable.forEach((num) => console.log(num * 2));
 //2,4
 
 //Built-in HOF- find() - returns first matched element | similary some(), every()
-const n = [1, 2, 3, 4, 5];
-const fEven = n.find((num) => num % 2 === 0);
+const list = [1, 2, 3, 4, 5];
+const fEven = list.find((num) => num % 2 === 0);
 console.log(fEven);
 
