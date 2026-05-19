e1bd9fb5134ac717af0df69bebe0b659de5d57cb
debdeep <debdeepm89@gmail.com>
2026-05-19 20:43:44 +0530

    negative number reverse case handling added

--- src/data-structure-algorithms/number-problems/reverseIntegerWithoutHelper.js
+++ src/data-structure-algorithms/number-problems/reverseIntegerWithoutHelper.js
@@ -1,21 +1,23 @@
 function reverseNumber(number) {
+    let nCopy = number; //copy of the original number
+    let n = Math.abs(number);
     let rev = 0;
-    let n = number;
-    if (number < 0) {
-        return "Negative Number can never be Palindrome..!";
-    }
-    if (number === 0) {
+
+    if (n === 0) {
         return 0;
     }
+
     while (n > 0) {
         rem = n % 10;
         rev = (10 * rev) + rem;
         n = Math.floor(n / 10);
     }
-    return rev;
+
+    return (nCopy < 0) ? -rev : rev;
 }
 
 //Outputs:
-console.log(reverseNumber(1234)); //4321
+console.log(reverseNumber(1234)); // 4321
+console.log(reverseNumber(121251)); // 152121
 console.log(reverseNumber(0)); // 0
-console.log(reverseNumber(-123)); // Negative Number can never be Palindrome..!
+console.log(reverseNumber(-123)); //
