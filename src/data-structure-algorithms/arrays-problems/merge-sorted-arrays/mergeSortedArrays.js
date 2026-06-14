b04151468410d6bab7757c145289e01a8d6c3a13
debdeep <debdeepm89@gmail.com>
2026-06-14 20:25:44 +0530

    merge sorted arrays combine with diff approaches

--- src/data-structure-algorithms/arrays-problems/merge-sorted-arrays/mergeSortedArrays.js
+++ src/data-structure-algorithms/arrays-problems/merge-sorted-arrays/mergeSortedArrays.js
@@ -1,9 +1,9 @@
 /*
-    Merge Sorted Arrays:
+    Merge Sorted Arrays (With a Copy):
     Two sorted arrays need to be merged into a single "sorted" one in "non-decreasing order" like having duplicates
     and the other array (nums2) will be of length 0 then.
-    nums1 = [1,2,3,4, 0,0,0,0]  -> m = 4  | nums2 = [4,5,6,7] -> n = 4 // m & n denotes to Length of arrays
-    nums1 = [1,2,3,4,4,5,6,7]
+    nums1 = [1, 2, 3, 4, 0, 0, 0, 0]  -> m = 4  | nums2 = [4, 5, 6, 7] -> n = 4 // m & n denotes to Length of arrays
+    Results - nums1 = [1, 2, 3, 4, 4, 5, 6, 7];
  */
 
 function mergeSortedArrays(nums1, m, nums2, n) {
@@ -12,7 +12,7 @@ function mergeSortedArrays(nums1, m, nums2, n) {
     let p2 = 0;
 
     for (let i = 0; i < m + n; i++) {
-        if (p2 >= n || (p1 < m && nums1_copy[p1] < nums2[p2])) {
+        if (p2 >= n || (nums1_copy[p1] < nums2[p2] && p1 < m)) {
             nums1[i] = nums1_copy[p1];
             p1++;
         } else {
@@ -20,10 +20,13 @@ function mergeSortedArrays(nums1, m, nums2, n) {
             p2++;
         }
     }
-    //return nums1;
+    return nums1;
 }
 
 //Outputs:
-console.log(mergeSortedArrays([1, 2, 3, 4], [2, 5, 6, 7, 8]));
-console.log(mergeSortedArrays([1, 2], [2, 5, 6, 7, 8]));
-console.log(mergeSortedArrays([1, 2], []));
+console.log(mergeSortedArrays([1, 2, 3, 4], 4, [2, 5, 6, 7, 8], 5)); // [1, 2, 2, 3, 4, 5, 6, 7, 8]
+console.log(mergeSortedArrays([1, 2], 2, [2, 5, 6, 7, 8], 5)); // [1, 2, 2, 5, 6, 7, 8]
+console.log(mergeSortedArrays([1, 2], 1, [], 0)); // [1, 2]
+
+//Shortcut Method - Brute force approach
+console.log([1, 2, 3, 4].concat([1, 2]).sort());
--- src/data-structure-algorithms/arrays-problems/merge-sorted-arrays/mergeSortedArraysInplace.js
+++ src/data-structure-algorithms/arrays-problems/merge-sorted-arrays/mergeSortedArraysInplace.js
@@ -1,3 +1,27 @@
-function mergeSortedArraysInplace(){
-    
+/*
+    Merge Sorted Arrays (Without a third Copy):
+    Two sorted arrays need to be merged into a single "sorted" one in "non-decreasing order" like having duplicates
+    and the other array (nums2) will be of length 0 then.
+    nums1 = [1, 2 ,3, 4, 0, 0, 0, 0]  -> m = 4  | nums2 = [4, 5, 6, 7] -> n = 4 // m & n denotes to Length of arrays
+    Results - nums1 = [1, 2, 3, 4, 4, 5, 6, 7];
+ */
+
+function mergeSortedArraysInplace(nums1, m, nums2, n) {
+    let p1 = m - 1, p2 = n - 1;
+    for (let i = (m + n - 1); i >= 0; i--) {
+        if (p2 < 0)
+            break;
+
+        if (p1>=0 && nums1[p1] > nums2[p2]) {
+            nums1[i] = nums1[p1];
+            p1--;
+        } else {
+            nums1[i] = nums2[p2];
+            p2--;
+        }
+    }
+    return nums1;
 }
+
+console.log(mergeSortedArraysInplace([1, 2, 3, 4], 4, [2, 5, 6, 7, 8], 5)); // [1, 2, 2, 3, 4, 5, 6, 7, 8]
+console.log(mergeSortedArraysInplace([1, 2], 1, [], 0)); // [1, 2]
