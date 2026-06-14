5c5d3736786faab10cab2eb26ea4e6f09286d912
debdeep <debdeepm89@gmail.com>
2026-06-14 21:26:37 +0530

    corner case edge case test added

--- src/data-structure-algorithms/arrays-problems/move-zeros/shiftZeroInPlace.js
+++ src/data-structure-algorithms/arrays-problems/move-zeros/shiftZeroInPlace.js
@@ -20,6 +20,7 @@ function shiftZeroInPlace(nums) {
 
 //Outputs:
 console.log(shiftZeroInPlace([1, 2, 3, 0, 0, 5, 9, 0])) // [1, 2, 3, 5, 9, 0, 0, 0]
+console.log(shiftZeroInPlace([1, 2, 3])) // [1, 2, 3]
 console.log(shiftZeroInPlace([0, 0, 0, 5, 9, 0])) // [5, 9, 0, 0, 0, 0]
 console.log(shiftZeroInPlace([1, 0])) // [1, 0]
 console.log(shiftZeroInPlace([0])) // [0]
