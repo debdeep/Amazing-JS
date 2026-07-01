1e7cb7be4e50c59e95e5a964f0f93eb47fb6610b
debdeep <debdeepm89@gmail.com>
2026-07-01 20:24:18 +0530

    logic added without helper

---
+++ src/data-structure-algorithms/string-problems/word-length/lastWordLengthWithoutHelper.js
@@ -0,0 +1,28 @@
+function lastWordLengthWithoutHelper(line) {
+    let n = line.length - 1;
+    let count = 0;
+    while (n >= 0) {
+        if (line[n] === " ") {
+            --n;
+        } else {
+            break;
+        }
+    }
+    // word character count
+    while (n >= 0) {
+        if (line[n] !== " ") {
+            --n;
+            count++;
+        } else {
+            break;
+        }
+    }
+    return count;
+
+}
+
+console.log(lastWordLengthWithoutHelper('I am Debdeep Majumder'));  // 8
+console.log(lastWordLengthWithoutHelper('  Debdeep Majumder  '));  // 8
+console.log(lastWordLengthWithoutHelper("Hello Sivaji")); // 6
+console.log(lastWordLengthWithoutHelper("")); // 0
+console.log(lastWordLengthWithoutHelper("Deba")); // 4 
