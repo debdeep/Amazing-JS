d1a871b19e84c843e08c8d793bb2252c4add2816
debdeep <debdeepm89@gmail.com>
2026-09-02 17:49:23 +0530

    bug fix for last element found in recursive fix

--- src/data-structure-algorithms/arrays-problems/find-charcter-index/findCharacterByIndexRecursive.js
+++ src/data-structure-algorithms/arrays-problems/find-charcter-index/findCharacterByIndexRecursive.js
@@ -1,6 +1,6 @@
 function findCharacterByIndex(inputArr, target, i = 0, result = []) {
     if (!target) return result; //always returns [] in case of no target element to find
-    if (i >= inputArr.length - 1) return result; //base case: when i reaches end of an array
+    if (i >= inputArr.length) return result; //base case: when i reaches end of an array
     if (inputArr[i].includes(target)) result.push(i);
 
     return findCharacterByIndex(inputArr, target, i + 1, result); //recursive call
@@ -9,5 +9,5 @@ function findCharacterByIndex(inputArr, target, i = 0, result = []) {
 //Output:
 console.log(findCharacterByIndex(["leet", "code", "match", "India"], "c"));
 console.log(findCharacterByIndex(["leet", "code", "match", "India"], ""));
-console.log(findCharacterByIndex(["leet", "code", "match", "India"], "e"));
+console.log(findCharacterByIndex(["leet", "code"], "e"));
 console.log(findCharacterByIndex(["leet", "code&code", "match", "India"], "&"));
