1d5e96c613a3ed63fed3e8d206577d3c2506af94
debdeep <debdeepm89@gmail.com>
2026-09-07 20:10:44 +0530

    group anagram using map added

---
+++ src/data-structure-algorithms/string-problems/group-anagrams/groupAnagram.js
@@ -0,0 +1,27 @@
+/*
+    Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    Constraints: 0 <= strs[i].length <= 100 | strs[i] consists of lowercase English letters.
+*/
+function groupAnagrams(strs) {
+    if (strs.length < 2) return [strs];
+
+    let map = {};
+    for (let word of strs) {
+        // Normalize: sort words by characters
+        let key = word.split("").sort().join("");
+        //console.log("key:", key);
+
+        if (!map[key]) {
+            map[key] = [];
+        }
+        map[key].push(word);
+    }
+    //console.log('map:', map);
+    return Object.values(map);  //returning only values as we need only grouped values
+}
+
+// Outputs:
+console.log(groupAnagrams([""])); // [[""]]
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));   // [["eat","tea","ate"],["tan","nat"],["bat"]]
+console.log(groupAnagrams(["a"])); // [["a"]]
+
